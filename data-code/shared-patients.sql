71644ae7e9682b71610347379d1cd5ddb17a6b29
Ian McCarthy <ianmccarthy.econ@gmail.com>
2021-08-26 17:22:56 -0400

    add years for sql query

--- data-code/shared-patients.sql
+++ data-code/shared-patients.sql
@@ -1,4 +1,9 @@
-DROP TABLE shared_30days_2009;
+------------------------------------------------
+-- 30 Day Shared Patients
+------------------------------------------------
+
+-- 2009
+DROP TABLE IF EXISTS shared_30days_2009;
 CREATE TABLE shared_30days_2009 (
 	npi1 VARCHAR(50),
 	npi2 VARCHAR(50),
@@ -7,5 +12,87 @@ CREATE TABLE shared_30days_2009 (
 	samedayvisits REAL);
 	
 COPY shared_30days_2009
-FROM '/home/immccar/SynologyDrive/Professional/Research Data/Physician Shared Patient Data/Physician Shared Patients 30-days/PSPD 2009.txt'
+FROM '/home/imccart/Professional/Research Data/Physician Shared Patient Data/Physician Shared Patients 30-days/PSPD 2009.txt'
+WITH DELIMITER ',';
+
+
+-- 2010
+DROP TABLE IF EXISTS shared_30days_2010;
+CREATE TABLE shared_30days_2010 (
+	npi1 VARCHAR(50),
+	npi2 VARCHAR(50),
+	paircount REAL,
+	benecount REAL,
+	samedayvisits REAL);
+	
+COPY shared_30days_2010
+FROM '/home/imccart/Professional/Research Data/Physician Shared Patient Data/Physician Shared Patients 30-days/PSPD 2010.txt'
+WITH DELIMITER ',';
+
+-- 2011
+DROP TABLE IF EXISTS shared_30days_2011;
+CREATE TABLE shared_30days_2011 (
+	npi1 VARCHAR(50),
+	npi2 VARCHAR(50),
+	paircount REAL,
+	benecount REAL,
+	samedayvisits REAL);
+	
+COPY shared_30days_2011
+FROM '/home/imccart/Professional/Research Data/Physician Shared Patient Data/Physician Shared Patients 30-days/PSPD 2011.txt'
+WITH DELIMITER ',';
+
+
+-- 2012
+DROP TABLE IF EXISTS shared_30days_2012;
+CREATE TABLE shared_30days_2012 (
+	npi1 VARCHAR(50),
+	npi2 VARCHAR(50),
+	paircount REAL,
+	benecount REAL,
+	samedayvisits REAL);
+	
+COPY shared_30days_2012
+FROM '/home/imccart/Professional/Research Data/Physician Shared Patient Data/Physician Shared Patients 30-days/PSPD 2012.txt'
+WITH DELIMITER ',';
+
+-- 2013
+DROP TABLE IF EXISTS shared_30days_2013;
+CREATE TABLE shared_30days_2013 (
+	npi1 VARCHAR(50),
+	npi2 VARCHAR(50),
+	paircount REAL,
+	benecount REAL,
+	samedayvisits REAL);
+	
+COPY shared_30days_2013
+FROM '/home/imccart/Professional/Research Data/Physician Shared Patient Data/Physician Shared Patients 30-days/PSPD 2013.txt'
+WITH DELIMITER ',';
+
+
+-- 2014
+DROP TABLE IF EXISTS shared_30days_2014;
+CREATE TABLE shared_30days_2014 (
+	npi1 VARCHAR(50),
+	npi2 VARCHAR(50),
+	paircount REAL,
+	benecount REAL,
+	samedayvisits REAL);
+	
+COPY shared_30days_2014
+FROM '/home/imccart/Professional/Research Data/Physician Shared Patient Data/Physician Shared Patients 30-days/PSPD 2014.txt'
+WITH DELIMITER ',';
+
+
+-- 2015
+DROP TABLE IF EXISTS shared_30days_2015;
+CREATE TABLE shared_30days_2015 (
+	npi1 VARCHAR(50),
+	npi2 VARCHAR(50),
+	paircount REAL,
+	benecount REAL,
+	samedayvisits REAL);
+	
+COPY shared_30days_2015
+FROM '/home/imccart/Professional/Research Data/Physician Shared Patient Data/Physician Shared Patients 30-days/PSPD 2015.txt'
 WITH DELIMITER ',';
